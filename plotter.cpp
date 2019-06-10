f112b9ca835ad29b973de82b18c9d1c31ba9e8ff
Atarico <luiz_paulo.09@hotmail.com>
2019-06-10 16:12:32 -0300

    Improved plotting aesthetics

--- plotter.cpp
+++ plotter.cpp
@@ -10,6 +10,7 @@
 #include "putvoxel.h"
 #include "cutvoxel.h"
 
+#include <algorithm>
 #include <vector>
 #include <QPainter>
 #include <QPen>
@@ -28,7 +29,7 @@ Plotter::Plotter(QWidget *parent) : QWidget(parent)
     figs.push_back(new PutSphere(10, 10, 10, 5, 0.1, 0.1, 0.9, 0.1));
     figs.push_back(new PutSphere(0, 10, 10, 5, 0.1, 0.1, 0.9, 0.1));
     figs.push_back(new PutSphere(10, 0, 10, 5, 0.1, 0.9, 0.1, 0.1));
-    figs.push_back(new PutSphere(0, 0, 10, 5, 0.1, 0.1, 0.9, 0.1));
+    figs.push_back(new PutSphere(0, 0, 10, 5, 1, 1, 1, 0.1));
 
     for(int i = 0; i< figs.size(); i++){
         figs[i]->draw(*sculptor);
@@ -75,24 +76,69 @@ void Plotter::paintEvent(QPaintEvent *event)
 
     // preparando a caneta
     // R, G, B
-    pen.setColor(QColor(0,0,0));
-    pen.setWidth(2);
+    pen.setColor(QColor(150,150,150));
+    pen.setWidth(4);
     // entregando a caneta ao pintor
     p.setPen(pen);
 
     // preparando o pincel
-    brush.setColor(QColor(255,255,255));
+    brush.setColor(QColor(250,250,250));
     brush.setStyle(Qt::SolidPattern);
     // entregando o pincel ao pintor
     p.setBrush(brush);
 
-    p.drawRect(0,0,width(),height());
+    p.drawRect(0,0,width(),height()); //painting the background rectangle
+
+    //defining our dashed line pattern after the background has been painted
+    QVector<qreal> dashes;
+    qreal space = 4;
+    dashes << 9 << space << 9 << space;
+    pen.setDashPattern(dashes);
+    pen.setDashOffset(3);
+    p.setPen(pen);
+
 
     //PAINTING 2D MATRIX
-    int sizeH = int(plane2D.size()); //horizontal size in pixels of our plotter
-    int sizeV = int(plane2D[0].size()); //vertical size in pixels of our plotter
-    float normSizeH = width()/sizeH;
-    float normSizeV = height()/sizeV;
+    int sizeH = int(plane2D.size()); //horizontal size of our matrix in voxels
+    int sizeV = int(plane2D[0].size()); //vertical size of our matrix in voxels
+    int smallerAxisPixels; //longest axis in relation to our plotter pixels
+    if(width() <= height()){
+        smallerAxisPixels = width();
+    }
+    else{
+        smallerAxisPixels = height();
+    }
+    int biggerAxisVoxels; //the longest axis in relation to our voxel matrix
+    if(sizeH >= sizeV){
+        biggerAxisVoxels = sizeH;
+    }
+    else{
+        biggerAxisVoxels = sizeV;
+    }
+    float normSize = smallerAxisPixels/biggerAxisVoxels; //a relation betwen our smaller pixel axis and our longest voxel axis to be used as a drawing unit.
+
+    QPoint *plottingStart = new QPoint(width()/2 - smallerAxisPixels/2, height()/2 - smallerAxisPixels/2); //defines the 2D point at which we will start painting our voxels.
+
+    for(int i=0; i<sizeH; i++)
+    {
+        for(int j=0; j<sizeV; j++)
+        {
+            if(!plane2D[i][j].isOn){
+                brush.setColor(QColor(240,240,240));
+                brush.setStyle(Qt::SolidPattern);
+                p.setBrush(brush);
+
+                pen.setColor(QColor(150,150,150));
+                pen.setWidth(1);
+                //pen.setStyle(Qt::DashLine);
+                p.setPen(pen);
+
+                p.drawRect(i*normSize + plottingStart->x(), j*normSize + plottingStart->y(),
+                           normSize, normSize);
+            }
+
+        }
+    }
 
     for(int i=0; i<sizeH; i++)
     {
@@ -102,15 +148,14 @@ void Plotter::paintEvent(QPaintEvent *event)
                 brush.setColor(QColor(plane2D[i][j].r*255,plane2D[i][j].g*255,plane2D[i][j].b*255));
                 brush.setStyle(Qt::SolidPattern);
                 p.setBrush(brush);
-                p.drawRect(i*normSizeH, j*normSizeV,
-                           i*normSizeH + normSizeH, j*normSizeV + normSizeV);
-            }
-            else{
-                brush.setColor(QColor(255,255,255));
-                brush.setStyle(Qt::SolidPattern);
-                p.setBrush(brush);
-                p.drawRect(i*normSizeH, j*normSizeV,
-                           i*normSizeH + normSizeH, j*normSizeV + normSizeV);
+
+                pen.setColor(QColor(0,0,0));
+                pen.setWidth(2);
+                pen.setStyle(Qt::SolidLine);
+                p.setPen(pen);
+
+                p.drawRect(i*normSize + plottingStart->x(), j*normSize + plottingStart->y(),
+                           normSize, normSize);
             }
 
         }
